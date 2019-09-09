08584da518db5e7f5eab30a27c90da77a36b5d16
Tim De Pauw <timdp@users.noreply.github.com>
2019-09-09 18:37:28 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Swirly
+
+A marble diagram generator.
+
+## Installation
+
+```bash
+npm install -g swirly
+```
+
+## Usage
+
+```bash
+swirly input.yml output.svg
+```
+
+## Author
+
+[Tim De Pauw](https://tmdpw.eu/)
+
+## License
+
+MIT
---
+++ bin/cli.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+const YAML = require('js-yaml')
+const yargs = require('yargs')
+const { draw } = require('../lib/')
+const fs = require('fs')
+
+const { _: filePaths, force } = yargs
+  .usage('$0 [-f] input output')
+  .demand(1)
+  .option('f', {
+    type: 'boolean',
+    alias: 'force',
+    description: 'Force overwrite'
+  })
+  .strict()
+  .parse()
+
+const inFilePath = filePaths[0]
+const outFilePath = filePaths[1] !== '-' ? filePaths[1] : null
+
+if (!force && outFilePath != null && fs.existsSync(outFilePath)) {
+  console.error(`File exists: ${outFilePath}`)
+  process.exit(2)
+}
+
+const inFileContents = fs.readFileSync(inFilePath, 'utf8')
+
+const spec = YAML.safeLoad(inFileContents)
+const { document } = draw(spec)
+const xml = document.toString()
+
+if (outFilePath != null) {
+  fs.writeFileSync(outFilePath, xml, 'utf8')
+} else {
+  console.info(xml)
+}
