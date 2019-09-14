d839d54b20195f9d993f1057cd82924b0525d234
Tim De Pauw <timdp@users.noreply.github.com>
2019-09-14 23:06:17 +0200

    Build examples in parallel

--- scripts/build-examples.sh
+++ scripts/build-examples.sh
@@ -7,7 +7,6 @@ cd examples
 for input in *.txt; do
   for ext in svg png; do
     output=${input/.txt/.$ext}
-    echo "$input -> $output"
-    node ../bin/cli -f --scale=400 "$input" "$output"
+    echo "$input" "$output"
   done
-done
+done | xargs -t -n2 -P4 node ../bin/cli -f --scale=400
