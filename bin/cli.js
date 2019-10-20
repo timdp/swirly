edb2bb182c1d7079431e325066d082fbb8a216cc
Tim De Pauw <timdp@users.noreply.github.com>
2019-10-20 13:47:06 +0200

    Optionally optimize SVG

--- bin/cli.js
+++ bin/cli.js
@@ -1,56 +1,102 @@
 #!/usr/bin/env node
 
+require('hard-rejection/register')
 const YAML = require('js-yaml')
+const SVGO = require('svgo')
 const yargs = require('yargs')
 const { drawMarbleDiagram, parseMarbleDiagramSpec } = require('../')
 const { rasterizeSvg } = require('../lib/rasterize')
 const fs = require('fs')
 const path = require('path')
 
-const { _: filePaths, force, scale } = yargs
-  .usage('$0 [-f] input output')
-  .demand(1)
-  .option('f', {
-    type: 'boolean',
-    alias: 'force',
-    description: 'Force overwrite'
-  })
-  .option('scale', {
-    type: 'number',
-    description: 'Image scale (not used for SVG)',
-    default: 100
-  })
-  .strict()
-  .parse()
+const YAML_EXTENSIONS = ['.yml', '.yaml']
+const RASTER_EXTENSIONS = ['.png', '.jpg']
 
-const inFilePath = filePaths[0]
-const outFilePath = filePaths[1] !== '-' ? filePaths[1] : null
+const getOpts = () => {
+  const { _: filePaths, force, optimize, scale } = yargs
+    .usage('$0 [-f] input output')
+    .demand(1)
+    .option('f', {
+      type: 'boolean',
+      alias: 'force',
+      description: 'Force overwrite'
+    })
+    .options('o', {
+      type: 'boolean',
+      alias: 'optimize',
+      description: 'Optimize SVG output using SVGO'
+    })
+    .option('z', {
+      type: 'number',
+      alias: 'scale',
+      description: 'Image scale (raster images only)',
+      default: 100
+    })
+    .strict()
+    .parse()
 
-if (!force && outFilePath != null && fs.existsSync(outFilePath)) {
-  console.error(`File exists: ${outFilePath}`)
-  process.exit(2)
+  const inFilePath = filePaths[0]
+  const outFilePath = filePaths[1] !== '-' ? filePaths[1] : null
+
+  return { inFilePath, outFilePath, force, optimize, scale }
 }
 
-const inFileContents = fs.readFileSync(inFilePath, 'utf8')
+const readSpec = inFilePath => {
+  const inFileContents = fs.readFileSync(inFilePath, 'utf8')
 
-const spec = ['.yml', '.yaml'].includes(path.extname(inFilePath))
-  ? YAML.safeLoad(inFileContents)
-  : parseMarbleDiagramSpec(inFileContents)
+  if (YAML_EXTENSIONS.includes(path.extname(inFilePath))) {
+    return YAML.safeLoad(inFileContents)
+  }
 
-const { xml, width, height } = drawMarbleDiagram(spec)
-
-if (outFilePath == null) {
-  console.info(xml)
-} else if (['.png', '.jpg'].includes(path.extname(outFilePath))) {
-  rasterizeSvg(
-    xml,
-    Math.round((width * scale) / 100),
-    Math.round((height * scale) / 100),
-    outFilePath
-  ).catch(err => {
-    console.error(err)
-    process.exit(3)
-  })
-} else {
-  fs.writeFileSync(outFilePath, xml, 'utf8')
+  return parseMarbleDiagramSpec(inFileContents)
 }
+
+const optimizeXml = async unoptXml => {
+  const svgo = new SVGO({ plugins: [{ removeViewBox: false }] })
+  const { data: optXml } = await svgo.optimize(unoptXml)
+  return optXml
+}
+
+const writers = [
+  {
+    match: outFilePath => outFilePath == null,
+    run: async xml => {
+      console.info(xml)
+    }
+  },
+  {
+    match: outFilePath => RASTER_EXTENSIONS.includes(path.extname(outFilePath)),
+    run: async (xml, outFilePath, width, height, scale) => {
+      await rasterizeSvg(
+        xml,
+        Math.round((width * scale) / 100),
+        Math.round((height * scale) / 100),
+        outFilePath
+      )
+    }
+  },
+  {
+    match: () => true,
+    run: async (xml, outFilePath) => {
+      fs.writeFileSync(outFilePath, xml, 'utf8')
+    }
+  }
+]
+
+const main = async () => {
+  const { inFilePath, outFilePath, force, optimize, scale } = getOpts()
+
+  if (!force && outFilePath != null && fs.existsSync(outFilePath)) {
+    console.error(`File exists: ${outFilePath}`)
+    process.exit(2)
+  }
+
+  const { run: write } = writers.find(({ match }) => match(outFilePath))
+
+  const spec = readSpec(inFilePath)
+  const { xml: unoptXml, width, height } = drawMarbleDiagram(spec)
+  const xml = optimize ? await optimizeXml(unoptXml) : unoptXml
+  await write(xml, outFilePath, width, height, scale)
+}
+
+main()
