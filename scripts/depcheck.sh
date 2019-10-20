9dae2f9eca95db3bce2d323db4aba81ae96d453a
Tim De Pauw <timdp@users.noreply.github.com>
2019-10-20 19:04:11 +0200

    Add depcheck

---
+++ scripts/depcheck.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -eo pipefail
+
+SELF=$0
+DIRNAME=$( dirname "$SELF" )
+ROOT=$DIRNAME/..
+DEPCHECK=$ROOT/node_modules/.bin/depcheck
+
+if [[ $# = 0 ]]; then
+  find "$ROOT/packages" -mindepth 1 -maxdepth 1 -type d -print0 |
+    xargs -0 -n 1 -P 8 "$SELF"
+  exit $?
+fi
+
+erroneous=0
+for pkgdir in "$@"; do
+  pkgname=$( basename "$pkgdir" )
+  missing=$( $DEPCHECK "$pkgdir" --json | jq -r '.missing | keys | join(" ")' )
+  if [[ -n $missing ]]; then
+    printf '%-32s %s\n' "$pkgname" "$missing" >&2
+    (( erroneous++ ))
+  fi
+done
+
+exit $erroneous
