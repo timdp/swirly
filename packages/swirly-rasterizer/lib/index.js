bc8b03564bc49071f5a40b0c112ad9470bb4857d
Tim De Pauw <timdp@users.noreply.github.com>
2019-10-21 13:43:36 +0200

    Export createHtml

--- packages/swirly-rasterizer/lib/index.js
+++ packages/swirly-rasterizer/lib/index.js
@@ -1,7 +1,9 @@
+const { createHtml } = require('./html')
 const { rasterizeSvg } = require('./rasterize')
 const { Screenshotter } = require('./screenshot')
 
 module.exports = {
+  createHtml,
   rasterizeSvg,
   Screenshotter
 }
