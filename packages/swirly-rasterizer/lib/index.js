917a3b6f23168492a6fa8405e4eddc19b74434a1
Tim De Pauw <timdp@users.noreply.github.com>
2019-10-21 13:06:00 +0200

    Export Screenshotter

--- packages/swirly-rasterizer/lib/index.js
+++ packages/swirly-rasterizer/lib/index.js
@@ -1,37 +1,7 @@
+const { rasterizeSvg } = require('./rasterize')
 const { Screenshotter } = require('./screenshot')
-const { createHtml } = require('./html')
 
-const safeDispose = async screenshotter => {
-  try {
-    await screenshotter.dispose()
-  } catch (_) {}
+module.exports = {
+  rasterizeSvg,
+  Screenshotter
 }
-
-const rasterizeSvg = async (svgXml, width, height) => {
-  const html = createHtml(svgXml, width, height)
-
-  const screenshotter = new Screenshotter()
-  try {
-    await screenshotter.init()
-  } catch (err) {
-    throw new Error(`Failed to start browser: ${err}`)
-  }
-
-  let imageData
-  try {
-    imageData = await screenshotter.capture(html, width, height)
-  } catch (err) {
-    await safeDispose(screenshotter)
-    throw new Error(`Failed to create screenshot: ${err}`)
-  }
-
-  try {
-    await screenshotter.dispose()
-  } catch (err) {
-    throw new Error(`Failed to dispose browser: ${err}`)
-  }
-
-  return imageData
-}
-
-module.exports = { rasterizeSvg }
