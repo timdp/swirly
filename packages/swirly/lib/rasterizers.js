7d632d64c589e5b0da03b29665307fcb3cd18c33
Tim De Pauw <timdp@users.noreply.github.com>
2019-11-10 12:39:37 +0100

    Exclude new rasterizers for now because they have issues

--- packages/swirly/lib/rasterizers.js
+++ packages/swirly/lib/rasterizers.js
@@ -1,11 +1,7 @@
-const { rasterizeSvg: cairo } = require('swirly-rasterizer-cairo')
-const { rasterizeSvg: imagemagick } = require('swirly-rasterizer-imagemagick')
 const { rasterizeSvg: inkscape } = require('swirly-rasterizer-inkscape')
 const { rasterizeSvg: puppeteer } = require('swirly-rasterizer-puppeteer')
 
 const rasterizers = {
-  cairo,
-  imagemagick,
   inkscape,
   puppeteer
 }
