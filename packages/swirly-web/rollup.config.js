ce3359e987e9f621b7a213ff65e43e987374135a
Tim De Pauw <timdp@users.noreply.github.com>
2020-04-29 18:43:54 +0200

    Bump dependencies

--- packages/swirly-web/rollup.config.js
+++ packages/swirly-web/rollup.config.js
@@ -1,7 +1,7 @@
-import json from 'rollup-plugin-json'
-import resolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-import babel from 'rollup-plugin-babel'
+import json from '@rollup/plugin-json'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import babel from '@rollup/plugin-babel'
 import { terser } from 'rollup-plugin-terser'
 
 export default {
