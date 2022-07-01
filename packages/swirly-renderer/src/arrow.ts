6c962e1e48990cc265fd5ed61698328b35747e07
Tim De Pauw <timdp@users.noreply.github.com>
2022-07-01 13:11:45 +0200

    Clarify some names in arrow renderer

--- packages/swirly-renderer/src/arrow.ts
+++ packages/swirly-renderer/src/arrow.ts
@@ -8,16 +8,16 @@ export const renderArrow = (
   arrowheadAngle: number,
   duration: number
 ): RendererResult => {
-  const y = streamHeight / 2
-  const width = duration * styles.frame_width! + streamHeight
+  const centerY = streamHeight / 2
+  const lineWidth = duration * styles.frame_width! + streamHeight
   const arrowheadWidth = arrowStyles.width!
   const arrowheadHeight =
     arrowheadWidth * Math.tan(degreesToRadians(arrowheadAngle / 2))
   const arrowHeadProtrusion =
     arrowStyles.stroke_width! / Math.sin(degreesToRadians(arrowheadAngle))
-  const y1 = y - arrowheadHeight
-  const y2 = y + arrowheadHeight
 
+  const strokeWidth = arrowStyles.stroke_width!
+  const strokeColor = arrowStyles.stroke_color!
   const fillColor = arrowStyles.fill_color!
   const filled = typeof fillColor === 'string' && fillColor !== ''
 
@@ -26,26 +26,28 @@ export const renderArrow = (
   $group.appendChild(
     createSvgElement(document, 'line', {
       x1: 0,
-      y1: y,
-      x2: width - (filled ? arrowheadWidth : 0),
-      y2: y,
-      stroke: arrowStyles.stroke_color!,
-      'stroke-width': arrowStyles.stroke_width!
+      y1: centerY,
+      x2: lineWidth - (filled ? arrowheadWidth : 0),
+      y2: centerY,
+      stroke: strokeColor,
+      'stroke-width': strokeWidth
     })
   )
 
+  const arrowheadTopY = centerY - arrowheadHeight
+  const arrowheadBottomY = centerY + arrowheadHeight
   const points = [
-    `${width - arrowheadWidth},${y1}`,
-    `${width},${y}`,
-    `${width - arrowheadWidth},${y2}`
+    `${lineWidth - arrowheadWidth},${arrowheadTopY}`,
+    `${lineWidth},${centerY}`,
+    `${lineWidth - arrowheadWidth},${arrowheadBottomY}`
   ]
 
   $group.appendChild(
     createSvgElement(document, filled ? 'polygon' : 'polyline', {
       points: points.join(' '),
       fill: filled ? fillColor : 'none',
-      stroke: arrowStyles.stroke_color!,
-      'stroke-width': arrowStyles.stroke_width!,
+      stroke: strokeColor,
+      'stroke-width': strokeWidth,
       'stroke-linecap': 'square'
     })
   )
@@ -53,7 +55,7 @@ export const renderArrow = (
   const bbox = {
     x1: 0,
     y1: 0,
-    x2: width + arrowHeadProtrusion,
+    x2: lineWidth + arrowHeadProtrusion,
     y2: streamHeight
   }
 
